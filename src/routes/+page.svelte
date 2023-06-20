f4ff9d52e349b86beee967362ec87214211a1dbc
dankernel <dkdkernel@gmail.com>
2023-06-20 18:47:49 +0900

    FIX: camera to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,115 +1,186 @@
 <script lang="ts">
-	import { Button, Gallery, Heading, Timeline, TimelineItem } from 'flowbite-svelte';
+	import { afterUpdate, onMount } from 'svelte';
 
-	const images = [
-		{ alt: 'erbology', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg' },
-		{ alt: 'shoes', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg' },
-		{ alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg' },
-		{ alt: 'plants', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg' },
-		{ alt: 'watch', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg' },
-		{ alt: 'shoe', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg' },
-		{ alt: 'cream', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg' },
-		{ alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg' },
-		{ alt: 'lamp', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg' },
-		{ alt: 'toiletbag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg' },
-		{
-			alt: 'playstation',
-			src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg'
-		},
-		{ alt: 'bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg' }
-	];
+	let apiEndpoint = 'https://vhi44p15j3.execute-api.ap-northeast-2.amazonaws.com/test/momp-od';
+	let buttonText = 'Capture and Send Image';
+	let data: any = null;
+
+	let cameraView: HTMLVideoElement | null = null;
+	let overlay: HTMLCanvasElement;
+
+	let cameras = [];
+	let selectedCameraId: string | undefined;
+
+	let isLandscape = false;
+
+	onMount(() => {
+		loadCameras();
+
+		mainInit();
+
+		checkOrientation();
+		window.addEventListener('orientationchange', checkOrientation);
+	});
+
+	afterUpdate(drawBoundingBoxes);
+
+	function camInit(stream: MediaStream): void {
+		cameraView = document.getElementById('cameraview') as HTMLVideoElement;
+		cameraView.srcObject = stream;
+		cameraView.onloadedmetadata = function () {
+			cameraView!.play();
+		};
+	}
+
+	function camInitFailed(error: any): void {
+		console.error('get camera permission failed : ', error);
+		alert('get camera permission failed');
+	}
+
+	async function mainInit(): Promise<void> {
+		if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+			alert('Media Device not supported');
+			return;
+		}
+
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: {
+					width: 1280,
+					height: 720,
+					facingMode: 'environment'
+				}
+			});
+			camInit(stream);
+		} catch (error) {
+			camInitFailed(error);
+		}
+	}
+
+	let color_list = ['#8622ff', '#fe0056', '#00ffce', '#ffff00', '#ff00ff', '#bc3989', '#b25050'];
+
+	function drawBoundingBoxes() {
+		if (!data || !overlay) {
+			console.log('pass');
+			return;
+		}
+
+		const context = overlay.getContext('2d');
+		context.clearRect(0, 0, overlay.width, overlay.height);
+		context.lineWidth = 10;
+
+		data.boxes.forEach(([x1, y1, x2, y2, conf, cla]) => {
+			context.strokeStyle = color_list[cla];
+
+			console.log(x1, y1, x2 - x1, y2 - y1);
+			context.strokeRect(x1, y1, x2 - x1, y2 - y1);
+		});
+	}
+
+	async function loadCameras() {
+		const devices = await navigator.mediaDevices.enumerateDevices();
+
+		cameras = devices.filter((device) => device.kind === 'videoinput');
+	}
+
+	async function startCamera() {
+		if (!selectedCameraId) return;
+
+		const constraints = {
+			video: {
+				width: 1280,
+				height: 720,
+				facingMode: 'environment',
+				deviceId: { exact: selectedCameraId }
+			}
+		};
+
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia(constraints);
+			const videoElement = document.getElementById('cameraview') as HTMLVideoElement;
+			videoElement.srcObject = stream;
+		} catch (err) {
+			console.error('Error accessing camera:', err);
+		}
+	}
+
+	function checkOrientation() {
+		isLandscape = window.matchMedia('(orientation: landscape)').matches;
+	}
+
+	async function captureAndSendImage(): Promise<void> {
+		if (!cameraView) return;
+
+		if (!cameraView.paused) {
+			cameraView.pause();
+		} else {
+			cameraView.play();
+			const context = overlay.getContext('2d');
+			context.clearRect(0, 0, overlay.width, overlay.height);
+			return;
+		}
+
+		const canvas = document.createElement('canvas');
+		canvas.width = cameraView.videoWidth;
+		canvas.height = cameraView.videoHeight;
+
+		const ctx = canvas.getContext('2d');
+		ctx!.drawImage(cameraView, 0, 0);
+
+		const imageBase64 = canvas.toDataURL('image/jpeg');
+		const base64Image = imageBase64.split(',')[1];
+
+		const response = await fetch(apiEndpoint, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ data: base64Image })
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		data = await response.json();
+	}
 </script>
 
-<div class="container">
-	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
-		Hello</Heading
-	>
-	<br />
+{#if isLandscape}
+	<div>
+		<div style="position: relative; width: 320px; height: 240px;">
+			<video
+				id="cameraview"
+				width="1280"
+				height="720"
+				autoplay
+				playsinline
+				bind:this={cameraView}
+				style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: contain;"
+			/>
 
-	<Heading tag="h2" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
-		Timeline</Heading
-	>
-	<Timeline order="vertical">
-		<TimelineItem title="Flowbite Application UI v2.0.0" date="Released on January 13th, 2022">
-			<svelte:fragment slot="icon">
-				<span
-					class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-primary-900"
-				>
-					<svg
-						aria-hidden="true"
-						class="w-3 h-3 text-primary-600 dark:text-primary-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-			</svelte:fragment>
-			<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-				Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar,
-				and pre-order E-commerce & Marketing pages.
-			</p>
-		</TimelineItem>
-		<TimelineItem title="Flowbite Figma v1.3.0" date="Released on December 7th, 2021">
-			<svelte:fragment slot="icon">
-				<span
-					class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-primary-900"
-				>
-					<svg
-						aria-hidden="true"
-						class="w-3 h-3 text-primary-600 dark:text-primary-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-			</svelte:fragment>
-			<p class="text-base font-normal text-gray-500 dark:text-gray-400">
-				All of the pages and components are first designed in Figma and we keep a parity between the
-				two versions even as we update the project.
-			</p>
-		</TimelineItem>
-		<TimelineItem title="Flowbite Library v1.2.2" date="Released on December 2nd, 2021">
-			<svelte:fragment slot="icon">
-				<span
-					class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-primary-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-primary-900"
-				>
-					<svg
-						aria-hidden="true"
-						class="w-3 h-3 text-primary-600 dark:text-primary-400"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-							clip-rule="evenodd"
-						/></svg
-					>
-				</span>
-			</svelte:fragment>
-			<p class="text-base font-normal text-gray-500 dark:text-gray-400">
-				Get started with dozens of web components and interactive elements built on top of Tailwind
-				CSS.
-			</p>
-		</TimelineItem>
-	</Timeline>
+			<canvas
+				id="overlay"
+				width="1280"
+				height="700"
+				bind:this={overlay}
+				style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: contain;"
+			/>
+		</div>
 
-	<br />
+		<input bind:value={apiEndpoint} type="text" placeholder="Enter API Endpoint" />
+		<button on:click={captureAndSendImage}>{buttonText}</button>
+		<pre>{JSON.stringify(data, null, 2)}</pre>
 
-	<Heading tag="h2" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
-		Gallery</Heading
-	>
-	<Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-3" />
-	<a href="/login"> <Button color="green">Login</Button></a>
-</div>
+		<select bind:value={selectedCameraId}>
+			<option disabled={true} selected={true}> -- select a camera --</option>
+			{#each cameras as camera (camera.deviceId)}
+				<option value={camera.deviceId}>{camera.label}</option>
+			{/each}
+		</select>
 
+		<button on:click={startCamera}>Start Camera</button>
+	</div>
+{:else}
+	<div>가로 모드로 전환해 주세요</div>
+{/if}
